7d9edf97720bdbe8281fa72e310992c31379f390
saloni-ship-it <salonisingh451@gmail.com>
2020-10-05 13:29:32 +0530

    pls

---
+++ src/test/java/Arman/Bangalore/ClothesTest.java
@@ -0,0 +1,14 @@
+package Arman.Bangalore;
+
+import org.testng.annotations.Test;
+
+public class ClothesTest {
+	
+	@Test
+	public void shirt()
+	{
+		System.out.println("shirt");
+		System.out.println("shirt");
+	}
+
+}
---
+++ src/test/java/Arman/Bangalore/mangoTest.java
@@ -0,0 +1,25 @@
+package Arman.Bangalore;
+
+import org.testng.annotations.Test;
+
+public class mangoTest {
+	
+	@Test
+	public void fruit()
+	{
+		System.out.println("fruit");
+	}
+	
+	@Test
+	public void banana()
+	{
+		System.out.println("banana");
+		System.out.println("banana2");
+		System.out.println("banana3");
+		System.out.println("banana");
+		System.out.println("banana2");
+		System.out.println("banana3");
+		
+	}
+
+}
