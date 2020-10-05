59b8b3d7ad9aa3c0f526eca749e78ecc41979ac0
saloni-ship-it <salonisingh451@gmail.com>
2020-10-05 13:39:59 +0530

    whatahaa

--- src/test/java/Arman/Bangalore/mangoTest.java
+++ src/test/java/Arman/Bangalore/mangoTest.java
@@ -13,12 +13,7 @@ public class mangoTest {
 	@Test
 	public void banana()
 	{
-		System.out.println("banana");
-		System.out.println("banana2");
-		System.out.println("banana3");
-		System.out.println("banana");
-		System.out.println("banana2");
-		System.out.println("banana3");
+		
 		System.out.println("banana");
 		System.out.println("banana2");
 		System.out.println("banana3");
